b92e7363d33de1784865e0dc451727bef7a43be8
Jobaier <engr.jobaieer@outlook.com>
2023-08-05 16:26:25 +0600

    Solution of D.

---
+++ D.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    long long A, B, C, D;
+    //Take Input from Keyboard for (A,B,C,D)
+    cin>> A >> B >> C >> D ;
+    //Print the result of the following equation (A*B)-(C*D)
+    cout<<"Difference = "<<(A*B)-(C*D);
+    return 0;
+}
