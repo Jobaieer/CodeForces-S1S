d94d2153fa21ec1b5b54577fe8c8235f8da9d6ba
Jobaier <engr.jobaieer@outlook.com>
2023-08-05 16:12:43 +0600

    first commit

---
+++ A.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    char name[100];
+    cin>>name;
+    cout<<"Hello, "<<name<<endl;
+    return 0;
+}
---
+++ B.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int ai;
+    long long xl;
+    char cn;
+    float fb;
+    double dy;
+    cin>> ai >> xl >> cn >> fb >> dy ;
+    cout<<ai<<endl<<xl<<endl<<cn<<endl<<fb<<endl<<dy<<endl;
+    return 0;
+}
