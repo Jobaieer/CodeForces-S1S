d8a8dc087c757b279f04324d093a27d523c2170e
Jobaier <engr.jobaieer@outlook.com>
2023-09-02 20:42:23 +0600

    Solution of B

--- B.cpp
+++ B.cpp
@@ -3,12 +3,12 @@ using namespace std;
 
 int main()
 {
-    int ai;
-    long long xl;
-    char cn;
-    float fb;
-    double dy;
-    cin>> ai >> xl >> cn >> fb >> dy ;
-    cout<<ai<<endl<<xl<<endl<<cn<<endl<<fb<<endl<<dy<<endl;
+    int AI;
+    long long XL;
+    char CN;
+    float FB;
+    double DY;
+    cin>> AI >> XL >> CN >> FB >> DY ;
+    cout<<AI<<endl<<XL<<endl<<CN<<endl<<FB<<endl<<DY<<endl;
     return 0;
 }
