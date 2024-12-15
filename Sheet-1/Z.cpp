0ea9cd4ff6dec23982184e0ed88d8c8f91413397
Jobaieer <jobaieer.cse@proton.me>
2024-12-16 03:00:22 +0600

    [Sheet-1] Z. Hard Compare

---
+++ Sheet-1/Z.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    long long int A, B, C, D;
+    cin >> A >> B >> C >> D;
+
+    if(B*log(A) > D*log(C)){
+        cout << "YES"<<endl;
+    }
+    else{
+        cout << "NO"<<endl;
+    }
+    return 0;
+}
